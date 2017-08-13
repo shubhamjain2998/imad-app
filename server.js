1e66e28f424164774e8edd2621933ddb595cc811
shubhamjain2998 <30628775+shubhamjain2998@users.noreply.github.com>
2017-08-13 23:22:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/second', function (req, res) {
+app.get('/second.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'second.html'));
 });
 
